524467129d396fb7f12239c9aebbe53cd77557bd
training01 <training01@WIN2008-DCTTS05-15062018.mlad.in>
2018-06-21 12:09:32 +0530

    commit

---
+++ Flightbooking/src/com/mav/model/FModel.java
@@ -0,0 +1,50 @@
+package com.mav.model;
+
+public class FModel {
+	private String fname;
+	private String src;
+	private String Dest;
+	private String fare;
+	private String seats;
+	private String aname;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getDest() {
+		return Dest;
+	}
+	public void setDest(String dest) {
+		Dest = dest;
+	}
+	public String getFare() {
+		return fare;
+	}
+	public void setFare(String fare) {
+		this.fare = fare;
+	}
+	public String getSeats() {
+		return seats;
+	}
+	public void setSeats(String seats) {
+		this.seats = seats;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	
+	
+
+	
+}
